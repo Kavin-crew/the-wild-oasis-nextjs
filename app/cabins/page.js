eab41dfe8fdd5d38dcc9b1a01107e5642fa87ce9
kavin-crew <abreganakavin.com>
2025-07-20 11:07:39 +0800

    fetching data

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,3 +1,17 @@
-export default function Page() {
-  return <div>cabins</div>;
+export default async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  return (
+    <div>
+      <h1>Cabins</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
